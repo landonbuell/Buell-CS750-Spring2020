446b677b47b5b9feb7676a1c6c101e43f356b1f8
landonbuell <lhb1007@wildcats.unh.edu>
2020-04-23 14:26:12 -0400

    23 April 2020
    - Progress on final project
    - Fixed dimensionality issuses
    - Weights are NOT updating
    - Baises are reach infinity after ~2 iters

--- Buell_Final_Project/Multilayer_Perceptron_v0/MLP_CLF_MAIN.py
+++ Buell_Final_Project/Multilayer_Perceptron_v0/MLP_CLF_MAIN.py
@@ -24,10 +24,19 @@ if __name__ == '__main__':
     print("X Shape:",X.shape)
     print("Classes:",np.unique(y).shape)
 
-    CLF_MODEL = MLP_utils.MLP_Classifier('JARVIS',layer_sizes=(10,),
-                                      n_features=4,n_classes=3)
+    CLF_MODEL = MLP_utils.MLP_Classifier('JARVIS',layer_sizes=(6,),
+                                      momentum=1,n_features=4,n_classes=3)
+
+    print(CLF_MODEL.weight_dims)
+    print(CLF_MODEL.bias_dims)
 
     CLF_MODEL = CLF_MODEL.train_model(X,y)  # train the model
 
+    CLF_MODEL.print_weights()
+
     y_pred = CLF_MODEL.predict(X)
     MLP_utils.confusion_matrix(CLF_MODEL,y,y_pred,show=True)
+
+
+    #plt.plot(CLF_MODEL.losses)
+    #plt.show()
--- Buell_Final_Project/Multilayer_Perceptron_v0/MLP_CLF_utilities.py
+++ Buell_Final_Project/Multilayer_Perceptron_v0/MLP_CLF_utilities.py
@@ -60,6 +60,7 @@ class MLP_Classifier ():
     """
 
     def __init__(self,name,layer_sizes,n_features,n_classes,
+                 momentum,
                  batch_size=100,max_iters=100):
         """ Initialize Object Instance """
         self.name = name                    # name of network
@@ -67,10 +68,11 @@ class MLP_Classifier ():
         self.depth = len(layer_sizes)       # number of hidden layers
         self.n_features = n_features        # number of input features
         self.n_classes = n_classes          # number of output classes
+        self.momentum = momentum            # scalar for SGD
         self.batch_size = batch_size        # sizes of mini-batches for training
         self.max_iters = max_iters          # maximum iterations over data
         # generate weighting matrices & bias vectors 
-        self.weight_dims,self.bais_dims = \
+        self.weight_dims,self.bias_dims = \
                 self.transformation_dims()      # dimensions for weight & bias arrays
         self.weights = self.weighting_matrices()# generate weighting matrices
         self.biases = self.bias_vectors()       # generate bias vectors
@@ -80,15 +82,21 @@ class MLP_Classifier ():
         """ Generate all dimesnions for all weighting & bias vectors """
         self.all_layers = np.insert(self.layer_sizes,[0,self.depth],
                                [self.n_features,self.n_classes])
+
         # arrays to hold dimesnions of arrays
         weight_dims = []
         bias_dims = []
-        
-        # iterate over all layers & assign shapes
-        for layer in range (0,len(self.all_layers)-1):
-            weight_dims.append((self.all_layers[layer+1],
-                                self.all_layers[layer]))
-            bias_dims.append((self.all_layers[layer+1]))
+
+        if self.layer_sizes == (0,):
+            weight_dims.append((self.n_classes,self.n_features))
+            bias_dims.append((self.n_classes))
+
+        else:        
+            # iterate over all layers & assign shapes
+            for layer in range (0,len(self.all_layers)-1):
+                weight_dims.append((self.all_layers[layer+1],
+                                    self.all_layers[layer]))
+                bias_dims.append((self.all_layers[layer+1]))
 
         return weight_dims,bias_dims
 
@@ -103,7 +111,7 @@ class MLP_Classifier ():
     def bias_vectors (self):
         """ Generate bias vectors to transform between layers """
         biases = []                             # list to hold bias vectors
-        for arr_shape in self.bais_dims:        # for each tuple
+        for arr_shape in self.bias_dims:        # for each tuple
             rand_vals = generate_random_array(arr_shape)    # generate params for that state
             biases.append(rand_vals)                        # add np arr to list
         return biases                           # return the list
@@ -119,16 +127,16 @@ class MLP_Classifier ():
 
     def forward_pass (self,x):
         """ Pass single sample through Network to last layer """
-        PRE_ACTS = [x]              # before act func is applied
-        FIN_ACTS = [x]              # hold all activations by layer      
+        PRE_ACTS = []              # before act func is applied
+        FIN_ACTS = []              # hold all activations by layer      
         for layer in range (0,len(self.all_layers)-1):
             # exact elements & reshape
             W = self.weights[layer]     # extract weight matrix
             b = self.biases[layer]      # extract bias vector
             # use FP equations
             z = W @ x + b       # pre-activation
-            PRE_ACTS.append(z)  # add to vec to list
-            x = sigmoid(z)      # apply act func, now next layer    
+            PRE_ACTS.append(z)  # add to vec to list              
+            x = 1*z             # "identity" activation
             FIN_ACTS.append(x)  # add vec to list
         # finished with all layers
         self.pre_activations = PRE_ACTS
@@ -153,6 +161,8 @@ class MLP_Classifier ():
 
         # iterate through data
         for I in range (self.max_iters):
+            print("Iteration:",I)
+            self.print_weights()
             shuffled = np.random.permutation(N_samples)
             X,Y = X[shuffled],Y[shuffled]       # shuffle data            
             X_batch,Y_batch = X[:batch_size],Y[:batch_size]
@@ -161,6 +171,7 @@ class MLP_Classifier ():
                 self.mini_batches(X_batch,Y_batch,batch_size)
 
             # update matrices based on gradients for mini-batch
+            self.losses = np.append(self.losses,batch_loss)
             for layer in range(len(self.weights)):
                 self.weights[layer] += weight_grads[layer]
                 self.biases[layer] += bias_grads[layer]
@@ -170,23 +181,24 @@ class MLP_Classifier ():
     def mini_batches (self,X_batch,Y_batch,batch_size):
         """ Iterate through samples in a mini-batch of data """
 
+        # Hold chnages in weights & biases for full mini-batch
         minibatch_weight_grads = [np.zeros(W.shape) for W in self.weights]
         minibatch_bias_grads = [np.zeros(b.shape) for b in self.biases]
 
         # In mini-batch subset:
         batch_loss_avg = 0
-        for x,y in zip(X_batch,Y_batch):
+        for x,y in zip(X_batch,Y_batch):        # one sample at a time
             self.forward_pass(x.transpose())    # forward pass
             output = self.activations[-1]       # network output
-            batch_loss_avg += RSS_Loss(output,y)# add loss val
+            batch_loss_avg += RSS_Loss(output,y)# compute sample loss
             # SGD ON SINGLE SAMPLE SGD(output,target)
-            dW,db = self.back_propagate(output,y)
-            for layer in range(len(self.weights)):   #add changes to grads for single samples
+            dW,db = self.back_propagate(output,y)   # compute gradients for single sample
+            for layer in range(len(self.weights)):  # add changes to grads for single samples
                 minibatch_weight_grads[layer] += dW[layer]
                 minibatch_bias_grads[layer] += db[layer]
         # Now, we have gone through all samples in the mini-batch
-        batch_loss_avg /= batch_size        # avg loss for this mini-batch
-        for layer in range(len(self.weights)):   
+        batch_loss_avg /= batch_size            # avg loss for this mini-batch
+        for layer in range(len(self.weights)):  # average dw & dW over batch size 
                 minibatch_weight_grads[layer] /= batch_size
                 minibatch_bias_grads[layer] /= batch_size
         # return avergaged bias & weight grads & avg loss for batch
@@ -194,22 +206,25 @@ class MLP_Classifier ():
 
     def back_propagate(self,x,y):
         """ Perform SGD Back-propagation given activations, weights & biases """
-        # hold activations
+        # hold activations (initialize w/ 0's)
         weight_grads = [np.zeros(W.shape) for W in self.weights]
         bias_grads = [np.zeros(b.shape) for b in self.biases]
-        dx = x - y                  # output - input
+        dx = 2*(x - y).transpose()                  # output - input
         for l in range (len(self.weights)-1,0,-1):
             # Change is biases , weights & next layer
-            
-            """ Need to iteratively enact back propagation
-            - Keep running into trouble with dimesnional issues
-            - Need to ensure calculus is correct
-            """
 
+            db = dx * self.pre_activations[l]       # change in baises
+            dW = dx.reshape(-1,1) @ self.activations[l-1].reshape(1,-1)        # change in weights
+            dx = self.weights[l].transpose() * db       # next layer
 
             weight_grads[l] += dW   # update weights
             bias_grads[l] += db     # update biases
         return weight_grads,bias_grads
+
+    def print_weights(self):
+        """ print out all weighting matrices """
+        for layer in self.weights:
+            print(layer)
             
 
             #### METRIC FUNCTIONS ####
